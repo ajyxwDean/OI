c82ad2821b065ee81fccfaa017b8cd9de40f4b8d
ajyxwDean <569488461@qq.com>
2023-07-27 14:54:26 +0800

    CatchCow AC

--- CatchCow.cpp
+++ CatchCow.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool vis[1000005];
+int n, k;
+struct node {
+    int x, step;
+};
+void bfs() {
+    queue<node> que;
+    que.push({n, 0});
+    vis[n] = 1;
+    while(!que.empty()) {
+        node n1 = que.front();
+        que.pop();
+        if(n1.x == k) {
+            cout << n1.step;
+            return ;
+        }else {
+            if(n1.x - 1 >= 0 && !vis[n1.x - 1]) {
+                vis[n1.x - 1] = 1;
+                que.push({n1.x - 1, n1.step + 1});
+            }
+            if(n1.x + 1 <= 100000 && !vis[n1.x + 1]) {
+                vis[n1.x + 1] = 1;
+                que.push({n1.x + 1, n1.step + 1});
+            }
+            if(n1.x * 2 <= 100000 && !vis[n1.x * 2]) {
+                vis[n1.x * 2] = 1;
+                que.push({n1.x * 2, n1.step + 1});
+            }
+        }
+    }
+}
 int main() {
-    
+    int n, k;
+    bfs();
     return 0;
 }
