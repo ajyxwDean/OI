4702c70e4d9eb31b02b03abcc309450ea3e9d589
ajxywDean <ajxywDean@outlook.com>
2024-02-12 18:35:37 +0800

    Modify : Setting.json Modify : c_cpp_properties.json Compile & Modify: P_2730.cpp Modify : 20240126.md

--- P_2730.cpp
+++ P_2730.cpp
@@ -20,9 +20,7 @@ void Solve();
 void bfs();
 void Update(string );
 
-string s, a, b, c;
-queue<string> q;
-map<string, string> mp;
+bool ok[8][8][8][8][8][8][8][8];
 
 signed main() {
     IOS();
@@ -31,51 +29,11 @@ signed main() {
 }
 
 void Solve() {
-    for(int i = 1; i <= 8; i++) {
-        char t = getchar();
-        s += t;
-        getchar();
-    }
+    
 //  _end -> _start
     bfs();
 }
 
 void bfs() {
-    q.push("12345678");
-    mp["12345678"] = "";
-    while (!q.empty()) {
-        /* code */
-        string t = q.front();
-        Update(t);
-        if(a == s) {
-            
-        }
-    }
-}
-
-void Update(string s) {
-    a += s[4];
-    a += s[5];
-    a += s[6];
-    a += s[7];
-    a += s[0];
-    a += s[1];
-    a += s[2];
-    a += s[3];
-    b += s[3];
-    b += s[0];
-    b += s[1];
-    b += s[2];
-    b += s[7];
-    b += s[4];
-    b += s[5];
-    b += s[6];
-    c += s[0];
-    c += s[6];
-    c += s[1];
-    c += s[3];
-    c += s[7];
-    c += s[5];
-    c += s[2];
-    c += s[4];
+    
 }
